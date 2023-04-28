04548eb3226f3975b68d9cd2d26d742fc738378b
rahulsharma <sharmarahul020899@gmail.com>
2023-04-28 10:42:58 +0530

    Added intial project till registeration and login functionality

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css'
+import Layout from './Layout';
+import IndexPage from './pages/IndexPage';
+import RegisterPage from './pages/RegisterPage.jsx';
+import LoginPage from './pages/LoginPage';
+import axios from 'axios';
+import { UserContextProvider } from './UserContext';
+
+axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.withCredentials = true;
+
+function App() {
+  return (
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+
+
+        </Route>
+
+
+      </Routes>
+
+
+    </UserContextProvider>
+
+  );
+}
+
+export default App;
